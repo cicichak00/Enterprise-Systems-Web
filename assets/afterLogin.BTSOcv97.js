9fd057ea8bed6320a5165bc68f36e6026fefa5d6
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-08-31 17:17:30 +0800

    feat: add revenue list export

---
+++ assets/afterLogin.BTSOcv97.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.Bb96bEDP.js";import{r as n}from"./index-DgWA1stb.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
