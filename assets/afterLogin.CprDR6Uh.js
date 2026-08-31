9f72d673ec7e754efd5e31360bd8cb3eaaa4744e
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-08-31 17:39:47 +0800

    fix: keep cached bundles and update staff actions

---
+++ assets/afterLogin.CprDR6Uh.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.BdoWha06.js";import{r as n}from"./index-DqIK_PFK.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
