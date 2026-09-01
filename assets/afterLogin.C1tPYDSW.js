9be8abe2775d59185bba2d5bca266763eaeb7a7b
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-09-01 14:40:11 +0800

    feat: align revenue calendar with local layout

---
+++ assets/afterLogin.C1tPYDSW.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.BmTrF4-m.js";import{r as n}from"./index-DLDBeNr-.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
