5f60bc5c0ce923fa7aa72fa9f39bbec0e1a918c8
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-09-01 15:55:51 +0800

    feat: separate ranking and revenue calendar flows

---
+++ assets/afterLogin.CUkqH1bb.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.CmFvR0aD.js";import{r as n}from"./index-BNKhgVtd.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
