27e16c8f9999575e457035f0b32928e2c80c31be
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-09-02 16:03:59 +0800

    fix: align settlement prompt with reference layout

---
+++ assets/afterLogin.CuI213JH.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.Q8rmGGW8.js";import{r as n}from"./index-DixhJ8pe.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
---
+++ assets/info.BgnCm2hZ.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-DixhJ8pe.js";function n(n="enterprise"){return e({url:"/api/v1.enterprise.AppConfig/info?action=".concat(n),method:"GET"})}export{n as g};
