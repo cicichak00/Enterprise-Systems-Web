fca5e50ddf4feca0f035c980ecba620d4ba64f16
语子酱 <yuzijiang@yuzijiangdeMacBook-Air.local>
2026-09-02 15:28:56 +0800

    fix: settlement prompt and revenue calendar data

---
+++ assets/afterLogin.BEmXzzG4.js
@@ -0,0 +1,1 @@
+import{n as i}from"./appSystemNavigation.B8Gub6mH.js";import{r as n}from"./index-DFGLTTln.js";function o(o,t=""){if(null!=o&&""!=o){const i=o.split("?")[0];if(!n.isLoginPage(i)&&n.isJoinInvitePage(i))return void n.nextLogin(o)}n.clearLoginRedirect(),i(t).finally(()=>{n.triggerAfterLogin()})}export{o as c};
---
+++ assets/info.DyvjByeL.js
@@ -0,0 +1,1 @@
+import{L as e}from"./index-DFGLTTln.js";function n(n="enterprise"){return e({url:"/api/v1.enterprise.AppConfig/info?action=".concat(n),method:"GET"})}export{n as g};
